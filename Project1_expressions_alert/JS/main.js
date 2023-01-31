67527722ea292c8c176170a79ca0bab69b21c766
noheM7 <122405800+noheM7@users.noreply.github.com>
2023-01-31 00:30:04 -0800

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("Hello, World!");
+
+var Sent1 = "This is the beginning of the string"; //variable 1
+var Sent2 = " and this is the end of the string "; //variable 2
+document.write(Sent1 + Sent2); //result
