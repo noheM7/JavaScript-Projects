c0e1c92c56aeff0b2f6448c6c0093954b1820794
Nohe <nmesfien@gmail.com>
2023-02-07 00:55:57 -0800

    Update tictactoe.js

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -53,7 +53,7 @@ function checkwinconditions(){
 
     if (arrayIncludes('0X','1X','2X')) {drawWinLine(50,100,558,100)}
     else if(arrayIncludes('3X','4X','5X')) {drawWinLine(50,304 ,558 ,304 )}
-    else if(arrayIncludes('6X','7X','8X')) {drawWinLine(50,304 ,558 ,508 )}
+    else if(arrayIncludes('6X','7X','8X')) {drawWinLine(50,508 ,558 ,508 )}
     else if(arrayIncludes('0X','3X','6X')) {drawWinLine(100,50 ,100 ,558 )}
     else if(arrayIncludes('1X','4X','7X')) {drawWinLine(304,50 ,304 ,558 )}
     else if(arrayIncludes('2X','5X','8X')) {drawWinLine(508,50 ,508 ,558 )}
@@ -103,12 +103,12 @@ function drawWinLine(coordx1, coordy1,coordx2, coordy2) {
 
     const c= canvas.getContext('2d');
 
-    let x1= coordx1,
-        y1=coordy1,
-        x2= coordx2,
-        y2= coordy2,
-        x= x1,
-        y=y1;
+    let x1 = coordx1,
+        y1 = coordy1,
+        x2 = coordx2,
+        y2 = coordy2,
+        x = x1,
+        y = y1;
 
 
 function animatelinedrawing(){
@@ -120,7 +120,7 @@ function animatelinedrawing(){
     c.moveTo(x1,y1);
     c.lineTo(x,y);
     c.lineWidth=10;
-    c.strokeStyle= 'rgba(70,255,33,.8)';
+    c.strokeStyle= 'rgba(70,255,33, .8)';
     c.stroke();
     if (x1<=x2 && y1<=y2){
         if(x<x2){x+=10}
